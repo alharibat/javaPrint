daa337ab91de11d030416746bd437109e4279901
mah2019 <mah2019@mahmoud-pc>
2021-11-01 12:20:08 +0200

    Fifth Commit

--- javaPrint/src/main/java/ju/edu/jo/Main.java
+++ javaPrint/src/main/java/ju/edu/jo/Main.java
@@ -3,7 +3,7 @@ package ju.edu.jo;
 public class Main {
 
 	public static void main(String[] args) {
-       System.out.println("hello world mahmoud");
+       System.out.println("hello world saeed");
 	}
 
 }
